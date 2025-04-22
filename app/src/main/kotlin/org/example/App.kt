b34861306bf0fbe3907a5bc7fa9fe49b6b7d2a3e
jmontoroetec <j.montoro@alumno.etec.um.edu.ar>
2025-04-22 12:15:18 -0300

    Naza - TP-1

---
+++ app/src/main/kotlin/org/example/App.kt
@@ -0,0 +1,11 @@
+package org.example
+
+import java.time.LocalDate
+fun main() {
+    val nombre = "Nazareno"
+    val fechadehoy = LocalDate.now()
+    
+    println("Mi nombre es $nombre")
+    println("Esta es la fecha de hoy $fechadehoy")
+}
+
