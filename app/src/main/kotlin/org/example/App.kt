e4531e860b7b6b72c5f0177a646fb674798f5e73
NazarenoMoreno1 <nc.moreno@alumno.etec.um.edu.ar>
2025-04-22 12:46:00 -0300

    Paso 2: Variables y Tipos de Datos

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -6,8 +6,13 @@ fun main() {
     println("Hola, Kotlin")
     val nombre = "Nazareno"
     val fechadehoy = LocalDate.now()
-    
     println("Mi nombre es $nombre")
     println("Esta es la fecha de hoy $fechadehoy")
+
+    val club: String = "Anzorena"
+    val camiseta: Int = 10
+    val puntos: Double = 7.6
+    val capitan: Boolean = true
+    println("Hola soy jugador del club $club, juego con la camiseta numero $camiseta, tengo un promedio de $puntos, puntos soy capitan de mi equipo: $capitan)
 }
 
